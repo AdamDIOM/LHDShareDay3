e6bf03fc71248a4b5f4b523224b506e4d00ef912
Adam Cameron Drummond <66436400+AdamDIOM@users.noreply.github.com>
2021-03-31 03:34:57 +0100

    added sort a list

---
+++ SortAList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortAList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Random rand = new Random();
+            for(int i = 0; i < 100; i++){
+                list.Add(rand.Next(0,100));
+            }
+            list.Sort();
+
+            foreach(int i in list){
+                Console.Write(i + ", ");
+            }
+        }
+    }
+}
